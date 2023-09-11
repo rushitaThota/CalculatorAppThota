a45c2866446d38b6d3c6cfb9a0778074159d12a7
rushitaThota <113718031+rushitaThota@users.noreply.github.com>
2023-09-11 11:58:28 -0500

    fix bug: final calculation is not showing up

--- app/src/main/java/com/example/calculatorappthota/MainActivity.java
+++ app/src/main/java/com/example/calculatorappthota/MainActivity.java
@@ -93,13 +93,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String getResult(String data){
         try{
             Context context = Context.enter();
+            //context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
             String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
             if(finalResult.endsWith(".0")){
                 finalResult = finalResult.replace(".0","");
             }
             return finalResult;
-        }catch (Exception e) {
+
+        }
+        catch (Exception e) {
             return "Error";
         }
 
